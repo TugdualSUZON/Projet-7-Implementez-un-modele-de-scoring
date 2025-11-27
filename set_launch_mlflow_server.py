6046adcd71fdfa5dd0fbb334f822bc6fb3e4fb71
TugdualSUZON <tugdual.suzon@gmail.com>
2025-11-27 13:55:31 +0100

    Premier dépôt "set_launch_mlflow_server" et "set_mlflow_tracking"
    Scripts pour initialiser mlflow. "set_launch_mlflow_server" n'est pas fonctionnel.

---
+++ set_launch_mlflow_server.py
@@ -0,0 +1,27 @@
+# File_name set_launch_mlflow_server.py
+# Author Tugdual SUZON 27/11/2025
+# Created 27/11/2025
+
+# Packages
+import mlflow
+import subprocess
+import time
+
+# Lancer le processus (ex : serveur MLflow)
+process = subprocess.Popen([
+    "mlflow", "server",
+    "--backend-store-uri", "sqlite:///data/mlflow.db",
+    #"--default-artifact-root", "file:///chemin/absolu/vers/data/mlruns",
+    "--host", "0.0.0.0",
+    "--port", "5000"
+])
+
+print("Serveur MLflow démarré.")
+
+#Bash
+# Launch : mlflow server --backend-store-uri sqlite:///mlflow.db --port 5000
+# Stop : mlflow server stop
+
+# Set :
+# Python
+#  mlflow.set_tracking_uri("http://localhost:5000")
